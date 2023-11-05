3441fc34b662432eb77afee5dcf16e2e8248de9b
Frank Wedlick <jfwedlick@gmail.com>
2023-11-05 11:06:19 -0500

    first commit

---
+++ Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    internal class Circle
+    {
+        public const double PI = Math.PI;
+
+        private double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateDiameter(double radius)
+        {
+            Console.WriteLine($"The diameter of the circle is {Math.Round(2 * radius, 3)}...");
+            return Radius;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            Console.WriteLine($"The circumference of the circle is {Math.Round(2 * PI * radius, 3)}...");
+            return Radius;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            Console.WriteLine($"The area of the circle is {Math.Round(Math.Pow(radius, 2) * PI, 3)}...");
+            return Radius;
+        }
+
+        public void Grow()
+        {
+            Radius *= 2;
+            CalculateDiameter(Radius);
+            CalculateCircumference(Radius);
+            CalculateArea(Radius);
+        }
+
+        public double GetRadius(double radius)
+        {
+            Radius = radius;
+            return radius;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Circle_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                RunProgram();
+            }
+            catch
+            {
+                Exception ex = new Exception();
+            }
+            finally
+            {
+                Console.WriteLine($"Now ending the program..."); // I don't really need this... but I like it.
+            }
+        }
+
+        static void RunProgram()
+        {
+            double radius = MakeCircle();
+            CalculateCircle(radius);
+        }
+        
+        static double MakeCircle()
+        {
+            while (true)
+            {
+                Console.Write("Hello! Please enter the radius of a circle: ");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (double.TryParse(userInput, out double radius))
+                {
+                    return radius;
+                }
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+
+        static void CalculateCircle(double radius)
+        {
+            Circle circle = new Circle(radius);
+            radius = circle.GetRadius(radius);
+
+            circle.CalculateDiameter(radius);
+            circle.CalculateArea(radius);
+            circle.CalculateCircumference(radius);
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Should the circle grow? Y/N: ");
+                string userInput = Console.ReadLine().Trim().ToUpper();
+
+                if (userInput == "Y")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Behold! The circle has grown:");
+                    Console.WriteLine();
+                    circle.Grow();
+                    radius *= 2;
+                    continue;
+                }
+                if (userInput == "N")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Goodbye! The circle's final radius is {radius}.");
+                    Console.WriteLine();
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        }
+    }
+}
